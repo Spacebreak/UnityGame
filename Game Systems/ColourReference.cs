0c12738837a577e6df1e3b13638faf19d93e83f2
Spacebreak <52688704+Spacebreak@users.noreply.github.com>
2021-09-08 18:44:22 +1000

    Delete Game Systems directory

--- Game Systems/ColourReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ColourReference : MonoBehaviour
-{
-    public Color[] tokenColours;    //A reference to the colour of the 6 tokens in the game.
-}
